2716fe67a8104e3c73fce8d6f2014addf5632896
seonhjeo <hoya011196@gmail.com>
2023-10-21 23:20:35 +0900

    implement voice chat

---
+++ Assets/Scripts/Player/PushToTalk.cs
@@ -0,0 +1,63 @@
+
+using System;
+using Photon.Voice.Unity;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PushToTalk : MonoBehaviour
+    {
+        private Recorder _recorder;
+        public InputActionReference pttKey;
+
+        private void Awake()
+        {
+            if (_recorder == null)
+            {
+                _recorder = GetComponent<Recorder>();
+            }
+
+            
+        }
+
+        private void OnEnable()
+        {
+            pttKey.action.performed += EnableTalking;
+            pttKey.action.canceled += DisableTalking;
+        }
+
+        private void OnDisable()
+        {
+            pttKey.action.performed -= EnableTalking;
+            pttKey.action.canceled -= DisableTalking;
+        }
+
+        private void EnableTalking(InputAction.CallbackContext obj)
+        {
+            _recorder.TransmitEnabled = true;
+        }
+        
+        private void DisableTalking(InputAction.CallbackContext obj)
+        {
+            _recorder.TransmitEnabled = false;
+        }
+
+
+        private void Update()
+        {
+            if (PlayerStats.Instance != null)
+            {
+                if (_recorder.TransmitEnabled && _recorder.VoiceDetector.Detected)
+                {
+                    PlayerStats.Instance.IsSpeaking = true;
+                }
+                else
+                {
+                    PlayerStats.Instance.IsSpeaking = false;
+                }
+            }
+        }
+    }
+}
+
